8dc5ca4057a5c7dbca04533daaef215dc0ec9851
mfpebrizal <ftm.pebrizal@gmail.com>
2017-03-30 11:04:49 +0700

    search using twitter API done

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,38 @@
 var express = require('express');
 var router = express.Router()
+var OAuth = require('oauth');
 
-var OAuth2 = OAuth.OAuth2;
-     var twitterConsumerKey = 'your key';
-     var twitterConsumerSecret = 'your secret';
-     var oauth2 = new OAuth2(server.config.keys.twitter.consumerKey,
-       twitterConsumerSecret,
-       'https://api.twitter.com/',
-       null,
-       'oauth2/token',
-       null);
-     oauth2.getOAuthAccessToken(
-       '',
-       {'grant_type':'client_credentials'},
-       function (e, access_token, refresh_token, results){
-       console.log('bearer: ',access_token);
-       done();
-     });
+require('dotenv').config()
 
+let twitterOauth = function (word,cb) {
+  var oauth = new OAuth.OAuth(
+        'https://api.twitter.com/oauth/request_token',
+        'https://api.twitter.com/oauth/access_token',
+        process.env.APPLICATION_CONSUMER_KEY,
+        process.env.APPLICATION_SECRET,
+        '1.0A',
+        null,
+        'HMAC-SHA1'
+      );
 
+  oauth.get(
+    // 'https://api.twitter.com/1.1/trends/place.json?id=23424977',
+    // 'https://api.twitter.com/1.1/search/tweets.json?f=users&q=hacktiv8&src=typd',
+    `https://api.twitter.com/1.1/search/tweets.json?q=${word}`,
+    process.env.USER_TOKEN, //test user token
+    process.env.USER_SECRET, //test user secret
+    function (e, data, res){
+      if (e) console.error(e);
+      console.log(require('util').inspect(data));
+      cb(data)
+    });
+}
+
+router.get('/search/:word', function (req,res,next) {
+  twitterOauth(req.params.word ,function (data) {
+    res.send(data)
+  })
+})
 
 
 module.exports = router

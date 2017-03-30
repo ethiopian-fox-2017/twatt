4c437b1c31daeda06c43b4d36149a7c4c7d04145
danilags <aguspermadi94@gmail.com>
2017-03-30 11:13:00 +0700

    env done

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var OAuth = require('oauth');
+require('dotenv').config();
 
 router.get('/', function(req, res, next) {
   var oauth = new OAuth.OAuth(
       'https://api.twitter.com/oauth/request_token',
       'https://api.twitter.com/oauth/access_token',
-      'SiYq9nzMC42ewHHOHRlXlvZM4',
-      'T5Jk03u3wxAkQKzyJCSMyZJrmsrRY4jWdrerz9jR75tWB2WPzZ',
+      process.env.CONSUMER_KEY,
+      process.env.APPLICATION_SECRET,
       '1.0A',
       null,
-      'HMAC-SHA1'
+      process.env.SECRET_WORD
     );
     oauth.get(
       'https://api.twitter.com/1.1/search/tweets.json?q=jakarta',
-      '847288380794716160-qeS9beZxxHAoFQ29Gj2vJwKv9gsAiu6', //test user token
-      'ys3t1pocSTtSnS5rtal9Es9q9jPnRsNF5rTjClH1dtP4L', //test user secret
+      process.env.USER_TOKEN, //test user token
+      process.env.USER_SECRET, //test user secret
       function (e, data){
         if (e) console.error(e);
           res.send(data)

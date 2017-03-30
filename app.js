5ca99436e03b082634c3ff6ae2ba4c276faa82ac
Anthony Juan <anthonyjuan95@gmail.com>
2017-03-30 11:04:27 +0700

    add .env

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const oauth = require('oauth');
+require('dotenv').config()
 
 var myOauth = new oauth.OAuth(
   'https://api.twitter.com/oauth/request_token',
   'https://api.twitter.com/oauth/access_token',
-  'AHgDiLQRKUkWaV5tVmHWueHuO',
-  'JDcmmA00BSuLvb3BDbvIxb5Q8K3kfgfa65rYDEXZbJKCHaf8EB',
+  process.env.CONSUMER_KEY,
+  process.env.CONSUMER_SECRET,
   '1.0',
   'http://127.0.0.1:3000/twitter/callback',
   'HMAC-SHA1',
@@ -14,9 +15,9 @@ var myOauth = new oauth.OAuth(
 );
 app.get('/search', (req, res) => {
   myOauth.get(
-        'https://api.twitter.com/1.1/search/tweets.json?q='+req.query.q,
-        '88395371-5g4YHhiLsUOdxMz8EkE99CxtcVxGXOO40UmgrXSLG', //test user token
-        'tvOIvXXxTyyVFJJ4nPg64OHzeEUmxgCW0sQugF3TFt2Lf', //test user secret
+        `https://api.twitter.com/1.1/search/tweets.json?q=${req.query.q}`,
+        process.env.TOKEN, //test user token
+        process.env.TOKEN_SECRET, //test user secret
         function(e, data) {
           if (e) console.error(e);
           res.send(data);
